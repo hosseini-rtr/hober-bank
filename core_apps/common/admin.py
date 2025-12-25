5a9863906d7bdf67be294fee640f1239a233a491
hosseini-rtr <h.hosseini.me@gmail.com>
2025-12-26 01:39:32 +0330

    Enhance ContentView model and admin interface with additional fields and permissions

--- core_apps/common/admin.py
+++ core_apps/common/admin.py
@@ -1,3 +1,57 @@
-from django.contrib import admin
+from typing import Any
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
+from django.http import HttpRequest
+from django.utils.translation import gettext_lazy as _
+
+from .models import ContentView
+
+
+@admin.register(ContentView)
+class ContentViewAdmin(admin.ModelAdmin):
+    list_display = [
+        "content_object",
+        "content_type",
+        "user",
+        "viewer_ip",
+        "last_viewed",
+        "created_at",
+    ]
+    list_filter = ["content_object", "content_type", "user", "viewer_ip", "last_viewed"]
+    date_hierarchy = "last_viewed"
+    readonly_fields = [
+        "content_object",
+        "object_id",
+        "content_type",
+        "user",
+        "viewer_ip",
+        "created_at",
+        "updated_at",
+    ]
+    fieldsets = (
+        (None, {"fields": ("content_type", "object_id", "content_object")}),
+        (_("View Details"), {"fields": ("user", "viewer_ip", "last_viewed")}),
+        (
+            _("Timestamp"),
+            {"fields": ("created_at", "updated_at"), "classes": ("collapse")},
+        ),
+    )
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        return False
+
+    def has_change_permission(
+        self, request: HttpRequest, obj: Any | None = ...
+    ) -> bool:
+        return False
+
+
+class ContentViewInline(GenericTabularInline):
+    model = ContentView
+    extra = 0
+    readonly_fields = ["user", "viewer_ip", "last_viewed", "cerated_at"]
+    can_delete = False
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        return False
